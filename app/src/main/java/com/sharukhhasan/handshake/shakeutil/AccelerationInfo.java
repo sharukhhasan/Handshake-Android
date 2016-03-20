65478670f58badf3595ad66ae2358381852c8e29
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2016-03-19 19:18:14 -0500

    adding class accelerationinfo

---
+++ app/src/main/java/com/sharukhhasan/handshake/shakeutil/AccelerationInfo.java
@@ -0,0 +1,10 @@
+package com.sharukhhasan.handshake.shakeutil;
+
+/**
+ * Created by Sharukh on 3/19/16.
+ */
+public class AccelerationInfo {
+    public long timestamp;
+    public boolean accelerating;
+    public AccelerationInfo next;
+}
